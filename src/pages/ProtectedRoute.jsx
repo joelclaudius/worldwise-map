13b572ffd05fa0dd7e75d5a80c632cb510ffb500
Claudius Joel <otiiclaudius@gmail.com>
2024-06-21 03:42:38 +0300

    Update of the app

---
+++ src/pages/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/FakeAuthContext";
+import { useEffect } from "react";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) navigate("/");
+  }, [isAuthenticated, navigate]);
+
+  return children;
+}
+
+export default ProtectedRoute;
